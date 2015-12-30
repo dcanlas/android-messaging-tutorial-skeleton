2b5424adeb3b7063763f38b06d13351048227f24
Dominique Canlas <dcanlas@gsngames.com>
2015-12-29 19:23:00 -0800

    adding wfl icon

--- MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/FoodGroup.java
+++ MessagingTutorialSkeleton/src/main/java/com/sinch/messagingtutorialskeleton/FoodGroup.java
@@ -3,10 +3,11 @@ package com.sinch.messagingtutorialskeleton;
 public class FoodGroup {
 
     public static FoodModel[] ALL_FOODS = {
-        new FoodModel(1, "burrito", "burrito_icon"),
-        new FoodModel(2, "burger", "burger_icon"),
-        new FoodModel(3, "pasta", "pasta_icon"),
-        new FoodModel(4, "sushi", "sushi_icon")
+        new FoodModel(1, "wfl", "wfl_icon"),
+        new FoodModel(2, "burrito", "burrito_icon"),
+        new FoodModel(3, "burger", "burger_icon"),
+        new FoodModel(4, "pasta", "pasta_icon"),
+        new FoodModel(5, "sushi", "sushi_icon"),
     };
 
 }
