a196adc56f012ca770f192ca4c30181dcbc7c1da
Dominique Canlas <canlasdominique@yahoo.com>
2016-01-03 18:01:39 -0800

    checkpoint - setup for fb auth, but it's not working yet

--- WFL/src/main/java/com/wfl/application/LoginActivity.java
+++ WFL/src/main/java/com/wfl/application/LoginActivity.java
@@ -2,27 +2,59 @@ package com.wfl.application;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
 import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 import com.waffle.wfl.R;
+
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
     private Button loginButton;
     private Button signUpButton;
+    private Button fbLoginButton;
     private EditText usernameField;
     private EditText passwordField;
 
     private Intent intent;
     private Intent serviceIntent;
+
+    private static String LOG_TAG = "LOGIN_ACTIVITY";
+
+    private String fbEmail;
+    private String fbName;
+    private Profile mFbProfile;
+
+    ProfilePhotoAsync mProfilePhotoAsync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +74,15 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 
         loginButton = (Button) findViewById(R.id.loginButton);
         signUpButton = (Button) findViewById(R.id.signupButton);
+        fbLoginButton = (Button) findViewById(R.id.fbLogin);
         usernameField = (EditText) findViewById(R.id.loginUsername);
         passwordField = (EditText) findViewById(R.id.loginPassword);
 
+        mFbProfile = Profile.getCurrentProfile();
+
         loginButton.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
+        fbLoginButton.setOnClickListener(this);
     }
 
     @Override
@@ -68,6 +104,9 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         }
         else if (view == signUpButton) {
             handleSignUp();
+        }
+        else if (view == fbLoginButton) {
+            handleFbLogin();
         }
     }
 
@@ -111,4 +150,139 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             }
         });
     }
+
+    private void handleFbLogin() {
+        List<String> mPermissions = Arrays.asList("public_profile", "user_about_me",
+                "user_friends", "email");
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, mPermissions, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+                if (user == null) {
+                    Log.d(LOG_TAG, "Uh oh. The user cancelled the Facebook login.");
+                } else if (user.isNew()) {
+                    Log.d(LOG_TAG, "User signed up and logged in through Facebook!");
+                    getUserDetailsFromFB();
+                } else {
+                    Log.d(LOG_TAG, "User logged in through Facebook!");
+                    getUserDetailsFromParse();
+                }
+            }
+        });
+    }
+
+    private void getUserDetailsFromFB() {
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+           /* handle the result */
+                        try {
+                            Log.d(LOG_TAG, response.getJSONObject().toString());
+                            Log.d(LOG_TAG, AccessToken.getCurrentAccessToken().getToken());
+                            fbEmail = response.getJSONObject().getString("email");
+//                            mEmailID.setText(email);
+                            fbName = response.getJSONObject().getString("name");
+//                            mUsername.setText(name);
+                            saveNewUser();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
+        mProfilePhotoAsync = new ProfilePhotoAsync(mFbProfile);
+        mProfilePhotoAsync.execute();
+    }
+
+    private void saveNewUser() {
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+        parseUser.setUsername(fbName);
+        parseUser.setEmail(fbEmail);
+
+//        Saving profile photo as a ParseFile
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        Bitmap bitmap = ((BitmapDrawable) mProfileImage.getDrawable()).getBitmap(); //Todo: set mprofileImage somewhere
+        Bitmap bitmap = mProfilePhotoAsync.bitmap;
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        byte[] data = stream.toByteArray();
+
+        String thumbName = parseUser.getUsername().replaceAll("\\s+", "");
+        final ParseFile parseFile = new ParseFile(thumbName + "_thumb.jpg", data);
+        parseFile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                parseUser.put("profileThumb", parseFile);
+                //Finally save all the user details
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(LoginActivity.this, "New user:" + fbName + " Signed up", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void getUserDetailsFromParse() {
+        ParseUser parseUser = ParseUser.getCurrentUser();
+        /* Don't need this right now
+        //Fetch profile photo
+        try {
+            ParseFile parseFile = parseUser.getParseFile("profileThumb");
+            byte[] data = parseFile.getData();
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            mProfileImage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+        fbEmail = parseUser.getEmail();
+        fbName = parseUser.getUsername();
+        Toast.makeText(LoginActivity.this, "Welcome back " + fbName, Toast.LENGTH_SHORT).show();
+    }
+
+    class ProfilePhotoAsync extends AsyncTask<String, String, String> {
+        Profile profile;
+        public Bitmap bitmap;
+
+        public ProfilePhotoAsync(Profile profile) {
+            this.profile = profile;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Fetching data from URI and storing in bitmap
+            bitmap = DownloadImageBitmap(profile.getProfilePictureUri(200, 200).toString());
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //image download is complete we can use the image in our view
+            super.onPostExecute(s);
+//            mProfileImage.setImageBitmap(bitmap);
+        }
+    }
+
+    public static Bitmap DownloadImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("IMAGE", "Error getting bitmap", e);
+        }
+        return bm;
+    }
+
 }
