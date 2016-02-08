4cb85b209980df313546bb5d4e0808be66d3544b
Dominique Canlas <canlasdominique@yahoo.com>
2016-02-07 21:17:13 -0800

    new signup activity

---
+++ WFL/src/main/java/com/wfl/application/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.wfl.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.waffle.wfl.R;
+
+public class SignupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+}
